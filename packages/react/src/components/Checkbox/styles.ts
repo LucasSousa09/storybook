e23415e3299fb4d3d4e4df81ede21c7e9e93560c
lucas <lucasrodrigues.sousa09@gmail.com>
2023-10-05 17:19:42 -0300

    fix: Add ref to TextInput and adjust styles

--- packages/react/src/components/Checkbox/styles.ts
+++ packages/react/src/components/Checkbox/styles.ts
@@ -19,7 +19,7 @@ export const CheckboxContainer = styled(Checkbox.Root, {
     border: '2px solid $gray900',
 
 
-    '&:focus': {
+    '&:focus, &[data-state="checked"]': {
         border: '2px solid $ignite300',
     },
 
