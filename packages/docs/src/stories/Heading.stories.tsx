824a5a93cf79b761e73ba689dc8e01c949bc3e6c
lucas <lucasrodrigues.sousa09@gmail.com>
2023-10-05 11:02:43 -0300

    Storybook example application

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,50 @@
+import type {Meta, StoryObj} from '@storybook/react'
+
+import { Heading, HeadingProps  } from '@ignite-ui/react'
+
+export default {
+    title: 'Typography/Heading',
+    component: Heading,
+
+
+    args:{
+        children: 'Custom Title',
+        size: 'md',
+    },
+    argTypes: {
+        size: {
+            options: [
+                'sm',
+                'md',
+                'lg',
+                '2xl',
+                '4xl',
+                '5xl',
+                '6xl',
+            ],
+            control: {
+                type: 'inline-radio',
+            }
+        },
+    },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+    args: {
+        children: 'H1 Heading',
+        as: 'h1',
+    },
+
+    parameters: {
+        docs: {
+            description: {
+                story: 
+                'Por padrão sempre será um `h1` mas é possível mudar isso com o `as`.',
+            },  
+        },
+    },
+}
+
+// parameters não apareceu por alguma razão
---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,32 @@
+import type {Meta, StoryObj} from '@storybook/react'
+
+import { Box, MultiSteps, MultiStepsProps  } from '@ignite-ui/react'
+
+export default {
+    title: 'Form/Multi Steps',
+    component: MultiSteps,
+    args: {
+        size: 4,
+        currentStep: 1,
+    },
+    decorators: [
+        (Story) => {
+            return (
+                <Box as="label" css={{display: 'flex', flexDirection: 'column', gap: '$2'}}>
+                    {Story()}
+                </Box>
+            )
+        }
+    ]
+
+} as Meta<MultiStepsProps>
+
+export const Primary: StoryObj<MultiStepsProps> = {
+    args: {}
+}
+
+export const Full: StoryObj<MultiStepsProps> = {
+    args: {
+        currentStep: 4
+    }
+}
